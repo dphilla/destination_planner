929655a345042e004ef90b514dfc8847470b96ad
Daniel Phillips <danieledward.phillips@gmail.com>
2017-10-31 18:14:20 -0600

    Add crud_request_spec

--- spec/requests/can_crud_spec.rb
+++ spec/requests/can_crud_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 describe "API" do
   it "can respond" do
 
+  #do the request and check it to be a success
+  #and check it to make sure the json response.body is what you want
 
 
   end
